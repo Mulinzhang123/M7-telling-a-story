537c81d16c4a763bc5755cb6141371438aa3e138
Mulinzhang123 <MuZhang@s.sfusd.edu>
2025-11-05 11:36:29 -0800

    L7--L11
    for this lesson i forgot to put my next lession into this so right now im on 11 which i forgot to put 8 9 10

---
+++ M07.telling_a_story_workbook/lessons/expressions.gd
@@ -0,0 +1,44 @@
+extends Control
+@onready var body: TextureRect = %body
+@onready var rowbodies: HBoxContainer = %rowbodies
+@onready var rowexpressions: HBoxContainer = %rowexpressions
+@onready var expression: TextureRect = %expression
+
+
+
+var bodies := {
+	"sophia": preload("res://assets/sophia.png"),
+	"pink": preload("res://assets/pink.png")
+}
+var expressions := {
+	"happy": preload("res://assets/emotion_happy.png"),
+	"regular": preload("res://assets/emotion_regular.png"),
+	"sad": preload("res://assets/emotion_sad.png"),
+}
+func create_button_pink() -> void:
+	var button := Button.new()
+	rowbodies.add_child(button)
+	var key := "pink"
+	button.text = key.capitalize()
+	button.pressed.connect(func() -> void:
+		body.texture = bodies[key]
+	)
+func _ready() -> void:
+		create_buttons()
+
+func create_buttons() -> void:
+	for current_body: String in bodies:
+		var button := Button.new()
+		rowbodies.add_child(button)
+		button.text = current_body.capitalize()
+		button.pressed.connect(func() -> void:
+			body.texture = bodies[current_body]
+		)
+	for current_expression: String in expressions:
+		var button := Button.new()
+		rowexpressions.add_child(button)
+		button.text = current_expression.capitalize()
+		button.pressed.connect(func() -> void:
+			expression.texture = expressions[current_expression]
+		)
+	
